af036c59a50bb848ad3b5fc390bfcd9fe92fd3e0
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-05-20 21:48:05 +0200

    Changed to new moves structure

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,10 @@
       <board :state="state"></board>
       <aside v-if="showControls" class="controls">
         <tags v-if="showTags" :tags="model.tags"></tags>
-        <moves v-if="showMoves" :moves="model.moves"></moves>
+        <moves v-if="showMoves"
+          :moves="model.moves"
+          :comments="model.comments"
+        ></moves>
       </aside>
     </div>
   </article>
@@ -36,7 +39,38 @@ export default {
   },
   data() {
     return {
-      tps: '',
+      tps: `[Event "First Video for Tak Strategy"]
+[Site "PlayTak.com"]
+[Date "2015.11.15"]
+[Round "1"]
+[Player1 "BenWo"]
+[Player2 "JT (Mongoose1021)"]
+[Size "5"]
+[Result "0-R"]
+
+1. e5 {comment1} a1
+2.  c3  d2 {comment2}
+3.  e3  Cd3
+4.  Cc2 {comment3} d4 {comment4}
+5.  c2> Sc2
+6.  Sd1 c4
+7.  a4  b4
+8.  a4> c4<
+9.  c5  c4
+10. c5- d4<
+11. c3+ 2b4>
+12. Sa4 c5
+13. b1  d3<
+14. 1d2< d5
+15. Sb5 c3+
+16. a4> a4
+17. b5> a5
+18. 2c5>11 b5
+19. 2e5< 4c4+
+20. 2b4> e5
+21. 4d5> 1c5-
+22. Sd5 3c4>12 0-R`,
+      selectedMove: 0,
     };
   },
   computed: {
--- src/components/controls/Moves.vue
+++ src/components/controls/Moves.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="moves">
-    <full-move :move="move" :number="$key" v-for="move in moves"></full-move>
+    <full-move :move="move" :number="$key" v-for="move in fullMoves"></full-move>
   </div>
 </template>
 
 <script>
 import FullMove from './FullMove';
+import { groupBy, map, forEach, assign } from 'lodash';
+
 export default {
   components: {
     FullMove,
   },
   props: {
     moves: Object,
+    comments: Object,
+  },
+  computed: {
+    fullMoves() {
+      const arr = map(this.moves, (move, key) => ({
+        key,
+        move,
+        comment: this.comments[key],
+      }));
+      forEach(arr, (fullMove, index) => {
+        assign(fullMove, { index });
+      });
+
+      return groupBy(arr, fullMove => fullMove.key.split(' ')[0]);
+    },
   },
 };
 </script>
