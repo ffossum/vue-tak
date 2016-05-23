b62b29f9ff5fd6a5fff986fbc5faa6e35cab706a
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-05-23 22:22:43 +0200

    Add color legend

--- src/components/controls/Tags.vue
+++ src/components/controls/Tags.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="game-info">
-    <div class="game-info-players">{{whitePlayer}} vs. {{blackPlayer}}</div>
+    <div class="game-info-players">
+      <div>
+        <div class="color white" title="White"></div> {{whitePlayer}}
+      </div>
+      <div>
+        <div class="color black" title="Black"></div> {{blackPlayer}}
+      </div>
+    </div>
     <dl>
       <template v-for="(key, value) in filteredTags">
         <dt>{{key}}</dt>
@@ -41,6 +48,27 @@ export default {
 
 .game-info-players {
   margin-bottom: 1rem;
+}
+
+.color {
+  height: 1rem;
+  width: 1rem;
+  position: relative;
+  top: 0.1rem;
+  display: inline-block;
+  border: 1px solid #333;
+  border-radius: 3px;
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),
+    0 3px 1px -2px rgba(0,0,0,.2),
+    0 1px 5px 0 rgba(0,0,0,.12);
+}
+
+.black {
+  background-color: #666;
+}
+
+.white {
+  background-color: white;
 }
 
 dl {
