57e6a33302ec0ef6c460a4a53cf99a4a9b055b62
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-05-23 22:07:41 +0200

    Player names in their own element

--- src/components/controls/Tags.vue
+++ src/components/controls/Tags.vue
@@ -1,22 +1,50 @@
 <template>
-  <dl>
-    <template v-for="(key, value) in tags">
-      <dt>{{key}}</dt>
-      <dd>{{value}}</dd>
-    </template>
-  </dl>
+  <div class="game-info">
+    <div class="game-info-players">{{whitePlayer}} vs. {{blackPlayer}}</div>
+    <dl>
+      <template v-for="(key, value) in filteredTags">
+        <dt>{{key}}</dt>
+        <dd>{{value}}</dd>
+      </template>
+    </dl>
+  </div>
 </template>
 
 <script>
- export default {
-   props: {
-     tags: Object,
-   },
- };
+import {
+  omit,
+} from 'lodash';
+
+export default {
+  props: {
+    tags: Object,
+  },
+  computed: {
+    whitePlayer() {
+      return this.tags.player1;
+    },
+    blackPlayer() {
+      return this.tags.player2;
+    },
+    filteredTags() {
+      return omit(this.tags, ['player1', 'player2']);
+    },
+  },
+};
 </script>
 
 <style scoped>
+.game-info {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+
+.game-info-players {
+  margin-bottom: 1rem;
+}
+
 dl {
+  margin: 0;
   display: flex;
   flex-wrap: wrap;
 }
